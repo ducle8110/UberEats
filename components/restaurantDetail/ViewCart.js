206adcae86df257859e38ca7a8297a7e0c8c4cb2
ducle <ducle8110@gmail.com>
2022-02-21 11:11:35 +0900

    last

--- components/restaurantDetail/ViewCart.js
+++ components/restaurantDetail/ViewCart.js
@@ -12,13 +12,11 @@ import {useSelector} from 'react-redux';
 import OrderItem from './OrderItem';
 import {Divider} from 'react-native-elements';
 import LottieView from 'lottie-react-native';
-import CarSchema from '../../Database/CarSchema';
-import PersonSchema from '../../Database/PersonSchema';
+import db from '../../database/db';
 
 export default function ViewCart({navigation}) {
   const [modalVisible, setModalVisible] = useState(false);
   const [loading, setLoading] = useState(false);
-  const Realm = require('realm');
 
   const {items, restaurantName} = useSelector(
     state => state.cartReducer.selectedItems,
@@ -50,69 +48,40 @@ export default function ViewCart({navigation}) {
           <View>
             <TouchableOpacity
               style={styles.checkout}
-              onPress={() => {
-                setLoading(true);
+              onPress={async () => {
+                try {
+                  setLoading(true);
 
-                // Initialize a Realm with Car and Person models
-                Realm.open({schema: [CarSchema, PersonSchema]}).then(realm => {
-                  // Add persons and their cars
-                  realm.write(() => {
-                    let john = realm.create('Person', {name: 'John', cars: []});
-                    john.cars.push({
-                      make: 'Honda',
-                      model: 'Accord',
-                      miles: 1500,
-                    });
-                    john.cars.push({
-                      make: 'Toyota',
-                      model: 'Prius',
-                      miles: 2780,
-                    });
+                  // db.info().then(function (info) {
+                  //   console.log(info);
+                  // });
+                  // db.get('mittens').then(function (doc) {
+                  //   console.log(doc);
+                  // });
 
-                    let joan = realm.create('Person', {name: 'Joan', cars: []});
-                    joan.cars.push({
-                      make: 'Skoda',
-                      model: 'Octavia',
-                      miles: 1120,
-                    });
-                    joan.cars.push({make: 'Ford', model: 'Fiesta', miles: 95});
-                    joan.cars.push({make: 'VW', model: 'Golf', miles: 1270});
+                  // var doc = {
+                  //   _id: 'mittens2',
+                  //   name: 'Mittens2',
+                  //   occupation: 'kitten2',
+                  //   age: 4,
+                  //   hobbies: [
+                  //     'playing with balls of yarn2',
+                  //     'chasing laser pointers2',
+                  //     "lookin' hella cute2",
+                  //   ],
+                  // };
+                  // db.put(doc);
 
-                    let jill = realm.create('Person', {name: 'Jill', cars: []});
+                  setTimeout(() => {
+                    setModalVisible(false);
+                    setLoading(false);
+                    navigation.navigate('OrderCompleted');
+                  }, 2500);
 
-                    let jack = realm.create('Person', {name: 'Jack', cars: []});
-                    jack.cars.push({make: 'Porche', model: '911', miles: 965});
-                  });
-
-                  // Find car owners
-                  let carOwners = realm
-                    .objects('Person')
-                    .filtered('cars.@size > 0');
-                  console.log('Car owners');
-                  for (let p of carOwners) {
-                    console.log(`  ${p.name}`);
-                  }
-
-                  // Find who has been driver longer than average
-                  let average = realm.objects('Car').avg('miles');
-                  let longerThanAverage = realm
-                    .objects('Person')
-                    .filtered('cars.@sum.miles > $0', average);
-                  console.log(`Longer than average (${average})`);
-                  for (let p of longerThanAverage) {
-                    console.log(`  ${p.name}: ${p.cars.sum('miles')}`);
-                  }
-
-                  realm.close();
-                });
-
-                setTimeout(() => {
                   setModalVisible(false);
-                  setLoading(false);
-                  navigation.navigate('OrderCompleted');
-                }, 2500);
-
-                setModalVisible(false);
+                } catch (ex) {
+                  console.log(ex);
+                }
               }}>
               <Text style={styles.checkoutText}>お会計に進む</Text>
             </TouchableOpacity>
---
+++ database/db.js
@@ -0,0 +1,5 @@
+import PouchDB from 'pouchdb-react-native';
+
+const db = new PouchDB('react-native-db');
+
+export default db;
